3d875f4637a3e849a46631638c5baf7070f60f57
Mizael <mizael.correia@gmail.com>
2023-01-17 18:31:10 -0300

    status

---
+++ Funcoes int e input/media.py
@@ -0,0 +1,9 @@
+"""
+Faça um programa que peça as 4 notas bimestrais e mostre a média
+"""
+
+soma = int(input("Digite a primeira nota: "))
+soma = soma + int(input("Digite a segunda nota: "))
+
+media = soma/4 #Queremos um numero real
+print("A media do aluno foi", media)
