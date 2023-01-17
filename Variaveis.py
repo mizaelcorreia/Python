bf39dbc014c1f03fa665c5b9733161af9cb75317
Mizael <mizael.correia@gmail.com>
2023-01-17 12:21:50 -0300

    Mostrando como lhe dar com variaveis dentro da função print

---
+++ Variaveis.py
@@ -0,0 +1,11 @@
+"Depois das aspas, por padrao o python adiciona um espaço"
+num = 11
+x = 10
+
+print("Eu tenho", num, "cachorros ,", x, "um morreu")
+
+print("\n")
+
+print(x)
+
+print("A cada",10, "que eu sei", 30, "eu nao sei")
