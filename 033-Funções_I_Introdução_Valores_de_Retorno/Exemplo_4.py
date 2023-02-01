8976912aa2c89cd3dd839cdfeeb3fee3f0a06bb0
Mizael <mizael.correia@gmail.com>
2023-02-01 20:53:09 -0300

    Introduzindo funcoes

---
+++ 033-Funções_I_Introdução_Valores_de_Retorno/Exemplo_4.py
@@ -0,0 +1,12 @@
+#4 - Função que não retorna nada
+"""
+Escreva uma função que sorteie um lançamento de dado e imprima
+o resultado
+"""
+import random
+
+def lançaDado():
+    a = random.randint(1,6)
+    print(a)
+
+lançaDado()
