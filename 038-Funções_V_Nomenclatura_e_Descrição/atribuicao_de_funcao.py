606dbe61e1d929b6ec571161fdbdff284145dada
Mizael <mizael.correia@gmail.com>
2023-02-04 16:18:27 -0300

    Nada demais

---
+++ 038-Funções_V_Nomenclatura_e_Descrição/atribuicao_de_funcao.py
@@ -0,0 +1,17 @@
+def f(ops):
+    num = ops
+    print(num)
+
+
+f(1)#chama a funcao com 1 como argumento em ops
+
+x = f #Atribui f a x 
+
+print(f)
+print(x)
+
+x(2)
+
+f = 3
+print(f)
+x(4)
---
+++ 038-Funções_V_Nomenclatura_e_Descrição/mesmos_nomes.py
@@ -0,0 +1,11 @@
+def f(ops):
+    f = ops
+    #num = ops
+    print(f)
+    #print(num)
+
+f(1)
+
+f = 2
+print(f)
+f(3)
