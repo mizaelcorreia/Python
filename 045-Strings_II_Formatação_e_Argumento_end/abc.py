6a1011929953b34ca5ff509e668d57943ecea002
Mizael <mizael.correia@gmail.com>
2023-02-07 00:28:54 -0300

    Tudo certo

---
+++ 045-Strings_II_Formatação_e_Argumento_end/abc.py
@@ -0,0 +1,9 @@
+print("Ola", end = ' ')
+print("Meu", end = ' ')
+print("Nome", end = ' ')
+print('é', end = ' ')
+
+"""
+a terminacao padradao do print é \n mas nesse caso foi alterado pra um espaço em branco
+
+"""
