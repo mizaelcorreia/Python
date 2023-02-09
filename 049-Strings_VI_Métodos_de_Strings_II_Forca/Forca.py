200b5a64b48bc9d0d110277151e20ea479babec6
Mizael <mizael.correia@gmail.com>
2023-02-09 19:58:47 -0300

    Forca

--- 049-Strings_VI_Métodos_de_Strings_II_Forca/Forca.py
+++ 049-Strings_VI_Métodos_de_Strings_II_Forca/Forca.py
@@ -179,4 +179,228 @@ def VerificaSeGanhou(palavraSecreta, letrasAcertadas):
     return ganhou
 
 main()
+
+"""
+F O R C A
+
+
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+
+Letras Erradas: 
+_ _ _ _ _ _ 
+Advinhe uma letra.
+w
+
+
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+
+Letras Erradas: W 
+_ _ _ _ _ _ 
+main()
+
+global FORCAIMG = é o desenho
+
+print('F O R C A')
+
+letrasErradas = ''
+
+letrasAcertadas = ''
+
+palavraSecreta = geraPalavraAleatória().upper()
+Palavra Global:
+
+palavras = 'formiga babuino encefalo elefante girafa hamburger chocolate giroscópio'.split()
+
+palavras = ['formiga', 'babuino', 'encefalo', 'elefante', 'girafa', 'hamburger', 'chocolate', 'giroscópio']
+1ª função: def geraPalavraAleatória():  Função que retorna uma string a partir da lista de palavras global  global palavras return random.choice(palavras)
+
+Digamos que foi escolhida aleatoriamente 'formiga'
+
+linha 72: palavraSecreta = geraPalavraAleatória().upper() palavraSecreta = 'FORMIGA'
+
+jogando = True
+while True:
+
+2ª função:
+
+def imprimeJogo(letrasErradas, letrasAcertadas, palavraSecreta):
+def imprimeJogo('', '', 'FORMIGA'):
+
+        global FORCAIMG
+    global FORCAIMG = desenhos da forca
+
+	    print(FORCAIMG[len(letrasErradas)]+'\n')
+    print(FORCAIMG[0]+ pula linha)
+
+''' +---+ | | | | | | ========='''
+
+        print("Letras Erradas:", end = ' ')
+    
+Tela:	Letras Erradas: W   --> só exemplo
+
+3ª função:
+
+       imprimeComEspaços(letrasErradas)
+
+       def imprimeComEspaços(''):
+d
+		for letra in palavra:
+	for '' in '':
+
+    	    print('', end = ' ')
+
+
+
+        vazio = '_'*len(palavraSecreta)
+    vazio = '_'*len('FORMIGA')
+    vazio = '_'*7
+
+
+        for i in range(len(palavraSecreta)):
+    for i in range(len('FORMIGA'):
+    for i=0 até 6:
+
+            if palavraSecreta[i] in letrasAcertadas:
+	if palavraSecreta[0] in '':
+	if 'F' in ''(False):
+		vazio = vazio[:i] + palavraSecreta[i] + vazio[i+1:]
+
+        imprimeComEspaços(vazio)
+    imprimeComEspaços('_'*7)
+
+Chama novamente a 3ª função:
+
+def imprimeComEspaços(palavra):
+def imprimeComEspaços('_'*7):
+
+        for letra in palavra:
+    for letra in '_'*7:
+
+    print(letra, end = ' ')
+
+Tela _ _ _ _ _ _ _
+
+
+	     print()
+
+volta para 2ª função finalizando a função imprimeJogo e voltando para main()
+
+palpite = recebePalpite(letrasErradas + letrasAcertadas)
+palpite = recebePalpite('' + '')
+palpite = recebePalpite('')
+
+chama 4ª função: def recebePalpite(palpitesFeitos): def recebePalpite(''):
+
+while True:
+    palpite = input("Advinhe uma letra.\n").upper()
+palpite = 'A'
+
+    if len(palpite) != 1:
+if 1 != 1(False):
+        print("Coloque uma única letra.")
+
+    elif palpite in ''(False):
+        print("Você já chutou esta letra. Escolha novamente.")
+
+    elif not 'A' <= palpite <= 'Z':
+elif not 'A' <= 'A' <= 'Z'(False): --> se não for verdade é True
+        print("Por favor escolha apenas letras.")
+
+    else:
+        return palpite
+    return 'A'
+
+Voltando para main(), linha 78
+
+palpite = 'A'
+
+	if 'A' in 'FORMIGA':
+	     letrasAcertadas = letrasAcertadas + palpite
+	     letrasAcertadas = '' + A
+	     letrasAcertadas = 'A'
+
+chama 5ª função: if VerificaSeGanhou(palavraSecreta, letrasAcertadas): if VerificaSeGanhou('FORMIGA', 'A'):
+
+			     ganhou = True
+			     for letra in palavraSecreta:
+		     for 'F' in 'FORMIGA':
+
+   				 if letra not in letrasAcertadas:
+			 if 'F' not in 'A'(True):
+
+      		             ganhou = False
+        			     break
+
+No break ele já sai direto da função para main()
+
+main() e verifica se a linha 83 é True, como não foi volta pra linha 98:
+
+if not jogando:
+if not True:
+if False:
+
+voltando pro While: while True:
+
+2ª função:
+
+def imprimeJogo(letrasErradas, letrasAcertadas, palavraSecreta):
+def imprimeJogo('', 'A', 'FORMIGA'):
+
+        global FORCAIMG
+    global FORCAIMG = desenhos da forca
+
+	    print(FORCAIMG[len(letrasErradas)]+'\n')
+    print(FORCAIMG[0]+ pula linha)
+
+''' +---+ | | | | | | ========='''
+
+        print("Letras Erradas:", end = ' ')
+    
+Tela:	Letras Erradas:    --> só exemplo
+3ª função:
+
+       imprimeComEspaços(letrasErradas)
+
+       def imprimeComEspaços(''):
+
+		for letra in palavra:
+	for '' in '':
+
+    	    print('', end = ' ')
+
+
+
+        vazio = '_'*len(palavraSecreta)
+    vazio = '_'*len('FORMIGA')
+    vazio = '_'*7
+
+
+        for i in range(len(palavraSecreta)):
+    for i in range(len('FORMIGA'):
+    for i=0 até 6:
+
+            if palavraSecreta[i] in letrasAcertadas:
+	if palavraSecreta[0] in 'A':
+	if 'A' in 'A'(False):
+		vazio = vazio[:6] + palavraSecreta[6] + vazio[7:]
+		vazio = '_' + A + ''
+
+
+        imprimeComEspaços(vazio)
+    imprimeComEspaços(_ _ _ _ _ _ A)
+
+
+"""
     
