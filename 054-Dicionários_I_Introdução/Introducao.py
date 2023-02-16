ce391313b82a7ef85b5942814ed396abee56f725
Mizael <mizael.correia@gmail.com>
2023-02-16 02:16:06 -0300

    A introduçao de dicionarios

---
+++ 054-Dicionários_I_Introdução/Introducao.py
@@ -0,0 +1,15 @@
+contato = {'Nome':'Pedro' , 'Telefone': '35017332' , 
+           'Celular':'70707070' , 'Email':'pedro.henrique.foli@usp.br' }
+
+a = contato['Nome']
+c= contato
+print(contato['Email'])
+print(a)
+
+b = Dicionario_de_contatos = {"Contato_1" : contato}
+
+print(b)
+
+contato['Endereco'] = 'Rua Direita'
+
+print(c)
